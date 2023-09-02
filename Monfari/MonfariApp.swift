2f6b2fbc7a1595798e249704eec88f85246e3b8d
bluepython508 <bluepython508@users.noreply.github.io>
2023-09-02 14:02:04 +0100

    Initial Commit

---
+++ Monfari/MonfariApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonfariApp.swift
+//  Monfari
+//
+//  Created by Ben on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonfariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
